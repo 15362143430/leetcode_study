ffacf9a0d0923f6822766695fe7b3b4045a4e9ed
linrui <857287645@qq.com>
2020-06-11 10:29:14 +0800

    环形链表多种

--- 141_环形链表.js
+++ 141_环形链表.js
@@ -20,3 +20,26 @@ var hasCycle = function(head) {
 
     return false;
 };
+
+
+// var hasCycle = (head) => {
+//   let fastP = head
+//   let slowP = head
+//   while (fastP) { // 当前快指针指向了节点
+//     if (!fastP.next) return false // 下一个为null了，没有环
+//     slowP = slowP.next // 快的前面都有节点，慢的前面当然有
+//     fastP = fastP.next.next // 推进2个节点
+//     if (slowP === fastP) return true // 快慢指针相遇，有环
+//   }
+//   return false // fastP为null了也始终不相遇
+// }
+
+// var hasCycle = function(head) {
+//     try{
+//         JSON.stringify(head);
+//         return false;
+//     }
+//     catch(err){
+//         return true;
+//     }
+// };
